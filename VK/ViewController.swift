d0b5f7b96cde7e2338d725a83d9597b081abc1ef
DaniStartsev <danistartsev@icloud.com>
2022-05-20 15:01:06 +0300

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Юлия Митрофанова on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
